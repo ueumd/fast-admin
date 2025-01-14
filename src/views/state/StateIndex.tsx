6df49dd6384f858f0d8363138d3d81b790616224
hsd <51134642@qq.com>
2025-01-14 20:35:26 +0800

    test

--- src/views/state/StateIndex.tsx
+++ src/views/state/StateIndex.tsx
@@ -1,9 +1,17 @@
+import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import TestJotai from './TestJotai'
 import TestZustnad from './TestZustand'
-import React from 'react'
+
+import { useUserinfoStore } from '@/store/zustand/userinfoStore.ts'
+import { useLocation } from 'react-router-dom'
+import { useAtom } from 'jotai/index'
+import { updateUserAtom } from '@/store/jotai/userinfoAtom.ts'
 
 const StateIndex = React.memo(() => {
+	const location = useLocation()
+	const user = useUserinfoStore(state => state.user)
+	const [userState, _] = useAtom(updateUserAtom)
 	return (
 		<div>
 			<ul>
@@ -14,6 +22,10 @@ const StateIndex = React.memo(() => {
 					<Link to={'zustand'}>zustand</Link>
 				</li>
 			</ul>
+			<div>
+				<code>{location.pathname === '/zustand' ? JSON.stringify(user) : JSON.stringify(userState)}</code>
+			</div>
+			<hr />
 			<Routes>
 				<Route path='jotai' element={<TestJotai />} />
 				<Route path='zustand' element={<TestZustnad />} />
